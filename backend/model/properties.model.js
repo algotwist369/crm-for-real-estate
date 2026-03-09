9e55b261fbe69386c34945481a5ba3b213fc972a
ankit pathak <info.algotwist@gmail.com>
2026-03-09 12:01:18 +0530

    install dependancy

--- backend/model/properties.model.js
+++ backend/model/properties.model.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const propertiesSchema = new Schema({
-    property_name:{
-        type:String,
-        required:true
+    property_name: {
+        type: String,
+        required: true
     }
 });
 
